9b8ae8b95595c7874303e05da62c177481b30306
YoussefJemmane <yous.jemm@gmail.com>
2025-07-22 11:17:48 +0100

    day3: fix task2

--- Day3/loops/task2.js
+++ Day3/loops/task2.js
@@ -1,8 +1,8 @@
 let num = 123542;
 let count = 0;
-
-while (num > 0 && num % 10 == 0){
-    console.log(num)
-    num = num / 10
+while (num !== 0){
     count++;
+    num = Math.floor(num / 10);
 }
+
+console.log(count)
